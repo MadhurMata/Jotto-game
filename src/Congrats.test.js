98d5670669462d52b5bc939a933a9e8ef9dc3e34
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-05-14 14:59:39 +0200

    congrats components and setuptests file

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import  Enzyme, { shallow }  from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-import { findByTestAttr } from '../test/testUtils';
+
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Congrats from './Congrats';
 
+const defaultProps = { success: false };
+
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
 
 /** 
  * Factory function to create a ShallowWrapper for the App component.
@@ -15,7 +19,8 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * @returns {ShallowWrapper}
  */
 const setup = (props={}) => {
-  return shallow(<Congrats {...props} />)
+  const setupProps = {...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test('renders without error', () => {
@@ -34,6 +39,10 @@ test('renders no empty congrats message when success prop is true', () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, 'congrats-message');
   expect(message.text().length).not.toBe(0);
-
-
 });
+
+test('does not throw warning with expected props', () => {
+  const expectedProps = { success: false};
+  checkProps(Congrats, expectedProps);
+});
+
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,4 @@
+import checkPropTypes from 'check-prop-types'
 
 /**
  * Return ShallowWrapper containing node(s) with the given dat-test value.
@@ -8,3 +9,12 @@
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`)
 };
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name);
+    expect(propError).toBeUndefined();
+}
